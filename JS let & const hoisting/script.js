97523e2a484fa9a51b6074291459ce4aca5e240c
Aneel Grewal <aneel.grewal@hotmail.com>
2024-04-02 23:46:50 +0200

    updated code

--- JS let & const hoisting/script.js
+++ JS let & const hoisting/script.js
@@ -1,5 +1,10 @@
-//console.log(a);
-
-let a;
+//console.log(a); //variable a is hoisted but can't access it before initialization
+console.log(b); //variable b is hoisted
+let a = 100;
 var b = 10;
 console.log(a);
+
+/* 
+var variables are hoisted in global scope 
+whereas let and const variables are hoisted in memory other than global scope i.e TDZ.
+ */
